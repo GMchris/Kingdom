5a4b9bf068008855ac55cd139a483b43aa618ee9
Kristian Ignatov <christianignatov@gmail.com>
2014-04-10 21:55:56 +0300

    In-game time implemented

---
+++ js/Events.js
@@ -0,0 +1,7 @@
+"use strict"
+
+// var Events = (function(){
+// 	function Attack(){
+// 		this.
+// 	}
+// })
--- js/Game.js
+++ js/Game.js
@@ -10,10 +10,12 @@ var GameControls = {
 
 	gameTimer:null,
 
-	dateTimer:null,
-
 	activateTimer:function(){
 		this.gameTimer = setInterval(function(){GameControls.gameLoop();},500);
+	},
+
+	deactivateTimer:function(){
+		clearInterval(GameControls.gameTimer)
 	},
 
 	setInitialValues: function(){
@@ -28,6 +30,13 @@ var GameControls = {
 	gameLoop: function(){
 		Structures.buildingManager.gatherResources();
 		cyclePlayer();
+		Time.tick();
+	},
+
+	loseGame:function(reason){
+		this.deactivateTimer();
+		$("<div>")
+		.addClass("prompt lossPrompt");
 	}
 }
 
@@ -39,6 +48,45 @@ var Player = {
 	food:null,
 	warriors:null
 }
+
+var Time={
+		ticks : 0,
+		day : 1,
+		month : 0,
+		season: 0,
+		months : ["March","April","May","June","July","August","September","October","November","December","January","February"],
+		seasons: ["Spring","Summer","Autumn","Winter"],
+		seasonModifiers:["Spring modifer","Summer modifer","Autumn modifer","Winter modifer"],
+
+		tick: function(){
+			this.ticks++;
+			if(this.ticks>=5){
+				this.ticks=0;
+				this.addDay()
+			}
+		},
+
+		addDay: function(){
+			if(this.month%2==0 && this.day==31||this.month%2 !=0 && this.day==30){
+				this.day=0;
+				this.addMonth();
+			}
+			this.day++;
+			$("#day").text(this.day);
+		},
+		addMonth:function(){
+			this.month++;
+			if(this.month==3||this.month==6||this.month==9){
+				this.addSeason();
+			}
+			$("#month").text(this.months[this.month]);
+		},
+		addSeason:function(){
+			this.seasons++;
+			$("#season").text(this.seasons[this.season]);
+			$("#seasonModifiers").text(this.seasonModifiers[this.season]);
+		},
+	}
 
 function cyclePlayer(){
 	var spots = $(".stats"),iterator=0;
@@ -52,7 +100,9 @@ window.addEventListener("load",GameControls.init,false);
 
 return {
 	playerStats:Player,
-	updatePlayerStats:cyclePlayer
+	updatePlayerStats:cyclePlayer,
+	activateTimer:GameControls.activateTimer,
+	deactivateTimer:GameControls.deactivateTimer
 }
 
 })();
