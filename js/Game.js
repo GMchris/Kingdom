de6f3bc2209b7d17ac8bf1f5e9fa43ae4364f700
GMchris <christianignatov@gmail.com>
2014-04-15 16:18:50 +0300

    Heck if I know

--- js/Game.js
+++ js/Game.js
@@ -24,7 +24,7 @@ var GameControls = {
 		Player.iron =10000;
 		Player.food =10000;
 		Player.gold =10000;
-		Player.citizens =2000;
+		Player.citizens =200;
 		Player.warriors =10000;
 	},
 
@@ -50,65 +50,6 @@ var Player = {
 	warriors:null
 }
 
-var Time={
-		total: 0,
-		ticks : 0,
-		day : 1,
-		month : 0,
-		season: 0,
-		months : ["March","April","May","June","July","August","September","October","November","December","January","February"],
-		seasons: ["Spring","Summer","Autumn","Winter"],
-		seasonModifiers:["Food from forester +100%","Gold +100%/Barn food +50%","Citizen grow +50%","Wood +50%/Food -100%"],
-		tick: function(){
-			this.total++;
-			this.ticks++;
-			if(this.ticks>=4){
-				this.ticks=0;
-				this.addDay()
-			}
-		},
-
-		addDay: function(){
-			if(this.month%2==0 && this.day==31||this.month%2 !=0 && this.day==30){
-				this.day=0;
-				this.addMonth();
-			}
-			this.day++;
-			$("#day").text(this.day);
-		},
-		addMonth:function(){
-			this.month++;
-			tax();
-			feed();
-			if(this.month==3||this.month==6||this.month==9){
-				this.addSeason();
-			}
-			$("#month").text(this.months[this.month]);
-		},
-		addSeason:function(){
-			this.seasons++;
-			$("#season").text(this.seasons[this.season]);
-			$("#seasonModifiers").text(this.seasonModifiers[this.season]);
-		},
-	}
-
-function tax(){
-	var taxThisMonth = Time.month*150;
-	if(Player.gold>=taxThisMonth){
-		Player.gold -= taxThisMonth;
-		return;
-	}
-	GameControls.loseGame("tax")
-}
-
-function feed(){
-	if(Player.food>=Player.citizens){
-		Player.food -= Player.citizens;
-		return;
-	}
-	GameControls.loseGame("food")
-}
-
 function cyclePlayer(){
 	var spots = $(".stats"),iterator=0;
 	$.each(Player,function(key,value){
@@ -124,7 +65,6 @@ return {
 	updatePlayerStats:cyclePlayer,
 	activateTimer:GameControls.activateTimer,
 	deactivateTimer:GameControls.deactivateTimer,
-	getSeason:Time.seasons[Time.season],
 }
 
 })();
